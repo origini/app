da8a1fdd6297cabfec9a7355f88d1671bfd5f096
tom <tom@sapletta.com>
2019-08-25 21:59:04 +0200

    update new solutions

--- visitor/home/js/include-url.js
+++ visitor/home/js/include-url.js
@@ -1,4 +1,4 @@
-function includeUrl(file, el_id, success, error) {
+function includeUrl(file, el_id, error, success) {
     var xhttp;
     var elmnt = document.getElementById(el_id);
 
@@ -25,14 +25,23 @@ function includeUrl(file, el_id, success, error) {
                 if (this.status == 200) {
                     // console.log('elmnt', elmnt);
                     // console.log('responseText', this.responseText);
-                    elmnt.innerHTML = this.responseText;
-                    success();
+                    // elmnt.innerHTML = this.responseText;
+                    // elmnt.appendChild(this.responseText);
+                    // elmnt.insertAdjacentHTML('beforeend', this.responseText);
+                    // var e = document.createElement('div');
+                    // e.innerHTML = this.responseText;
+                    // while(e.firstChild) {
+                    // elmnt.appendChild(e);
+                    // }
+
+                    // elmnt.insertAdjacentHTML('afterend', this.responseText);
+                    elmnt.insertAdjacentHTML('beforeend', this.responseText);
+
+                    success(this);
                 }
                 if (this.status == 404) {
                     elmnt.innerHTML = "Page not found.";
-
-                    error();
-
+                    error(this);
                 }
                 /* Remove the attribute, and call this function once more: */
                 // includeUrl(file, success, error);
