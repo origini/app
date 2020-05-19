b91a490c7e2756a910892dac1e3fabdc2704e178
tom <tom@sapletta.com>
2020-05-19 16:41:48 +0200

    update

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use('/', router);
 
 var server = app.listen(3000, '0.0.0.0', function () {
 
-    var host = server.address().address;
+    // var host = server.address().address;
+    var host = "localhost";
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
